37b04561842f587b77d8e640e2a4cd7a2d58b336
Oliver Enes <oliverenes01@gmail.com>
2018-07-29 03:57:06 +0200

    Added Logout

--- assets/frontend/src/Components/Header.vue
+++ assets/frontend/src/Components/Header.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <div class="page-header header">
-            <h3 class="font">ECOR - Enes Capture Of Resources</h3>
+            <h3 class="font header-child">ECOR - Enes Capture Of Resources</h3>
+            <i v-on:click="logout" class="fas fa-sign-out-alt fa-2x header-child logout" style="display: inline;"></i>
         </div>
     </div>
 </template>
@@ -9,10 +10,14 @@
 <script lang="ts">
     import Vue from "vue";
     import {Component} from "vue-property-decorator";
+    import LogoutBox from "./LogoutBox";
 
-    @Component
+    @Component({components: {LogoutBox}})
     export default class Header extends Vue
     {
+        public logout() {
+            window.location.href = window.location.origin  + "/logout";
+        }
     }
 </script>
 
@@ -25,7 +30,26 @@
         border-bottom: 1px solid rgba(0, 0, 0, 0.3);
     }
 
+    .header-child {
+        display: inline;
+    }
+
+    .logout {
+        float: right;
+        margin-right: 10px;
+        margin-top: 2px;
+        cursor: pointer;
+    }
+
+    .logout:hover {
+        color: rgba(0, 0, 0, 0.7);
+    }
+
     .font {
         font-family: "Open Sans", sans-serif;
+    }
+
+    texts {
+        text-align: center;
     }
 </style>
